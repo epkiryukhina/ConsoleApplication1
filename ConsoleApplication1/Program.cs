33c9524f0776cdf0539872b25635258ff05c561f
Xiaomi <Xiaomi@DESKTOP-02SFNU6>
2017-09-06 20:52:20 +0500

    1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Задание11
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ваш текст: ");
+            string str = Console.ReadLine();
+            string result = "";
+
+            for (int i = 0; i < str.Length; i++)
+                result += char.ConvertFromUtf32((char.ConvertToUtf32(str, i) + 1));
+
+            Console.Write(@"Выш зашифрованный текст: 
+" + result);
+            Console.WriteLine();
+
+            str = result;
+            result = "";
+
+            for (int i = 0; i < str.Length; i++)
+                result += char.ConvertFromUtf32((char.ConvertToUtf32(str, i) - 1));
+
+            Console.Write(@"Выш расшифрованный текст:
+" + result);
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
